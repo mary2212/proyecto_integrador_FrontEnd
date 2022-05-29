bdbefaebb9c775988e5d7d86565374497789b9b8
mary22 <magallanes2212@gmail.com>
2022-05-29 15:38:31 -0500

    validaciones

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup} from "@angular/forms"
+import { FormBuilder, FormGroup, Validators} from "@angular/forms"
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,16 +10,30 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+
+
   public loginForm !: FormGroup
   constructor(private formBuilder : FormBuilder, private http : HttpClient, private router : Router) { }
 
+  
+ /*cambio*/ submitted=false;/*cambio*/
+
   ngOnInit(): void {
     this.loginForm = this.formBuilder.group({
-      userId:[''],
-      password:['']
+      userId:[''/*cambio*/,[Validators.required]/*cambio*/],
+      password:[''/*cambio*/,[Validators.required]/*cambio*/]
     })
+
+  
   }
   login(){
+    /*cambio*/
+    this.submitted= true;
+
+    if(this.loginForm.invalid){
+      return;
+    }
+    /*cambio*/
     this.http.get<any>("http://localhost:3000/signupUsers")
     .subscribe(res=>{
       const userr = res.find((a:any)=>{
